c82378b004abfe76c07885034e2023db893d3d66
Doreapp <Doreapp@users.noreply.github.com>
2020-12-17 13:59:35 +0100

    Creating Class to Browse Facebook folder + Implementing facebook folder choice

--- app/src/main/java/com/example/test/mydataanalyser/utils/Constants.kt
+++ app/src/main/java/com/example/test/mydataanalyser/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.example.test.mydataanalyser.utils
 object Constants {
     const val REQUEST_CODE_READ_EXTERNAL_STORAGE_PERMISSION: Int = 1001
 
-    const val REQUEST_CODE_PICK_JSON_FILE: Int = 2001
+    const val REQUEST_CODE_PICK_FACEBOOK_FOLDER: Int = 2001
 
 
 }

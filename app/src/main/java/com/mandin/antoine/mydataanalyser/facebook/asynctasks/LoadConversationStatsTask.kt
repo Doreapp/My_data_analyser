fdc2c060e6055bc365a40bd71e47658096da9964
Doreapp <Doreapp@users.noreply.github.com>
2020-12-23 13:50:36 +0100

    Display progress in *Loading Dialog* of conversation Activity

--- app/src/main/java/com/mandin/antoine/mydataanalyser/facebook/asynctasks/LoadConversationStatsTask.kt
+++ app/src/main/java/com/mandin/antoine/mydataanalyser/facebook/asynctasks/LoadConversationStatsTask.kt
@@ -10,6 +10,7 @@ import com.mandin.antoine.mydataanalyser.facebook.model.data.ConversationData
 import com.mandin.antoine.mydataanalyser.facebook.model.data.ConversationStats
 import com.mandin.antoine.mydataanalyser.facebook.parsers.MessagesParser
 import com.mandin.antoine.mydataanalyser.tools.TaskObserver
+import com.mandin.antoine.mydataanalyser.utils.Debug
 import java.util.concurrent.Callable
 
 class LoadConversationStatsTask(
@@ -17,18 +18,22 @@ class LoadConversationStatsTask(
     private val dbHelper: FacebookDbHelper,
     private val conversationId: Long,
     private val observer: TaskObserver?
-) : Callable<LoadConversationStatsTask.Result?> {
-    private val findConversationProgressCost = 5
+) : Callable<LoadConversationStatsTask.Result> {
     private var result = Result()
 
-    override fun call(): LoadConversationStatsTask.Result? {
-        observer?.notify("Searching conversation...")
+    override fun call(): LoadConversationStatsTask.Result {
+        Debug.i(TAG, "call()")
+        observer?.setMaxProgress(findStatsCost + listMessagesCost + searchConversationCost)
+        observer?.notify("Searching for conversation...")
         dbHelper.findConversationById(conversationId)?.let {
+            observer?.notifyProgress(searchConversationCost)
             result.conversationData = it
             findConversation(it)
 
             result.conversation?.let { conversation ->
+                observer?.notify("Building statistics...")
                 result.conversationStats = ConversationStats(conversation)
+                observer?.notifyProgress(searchConversationCost + listMessagesCost + findStatsCost)
             }
         }
         return result
@@ -36,20 +41,31 @@ class LoadConversationStatsTask(
 
     // TODO progress
     private fun findConversation(conversationData: ConversationData) {
+        Debug.i(TAG, "findConversation() conversationData=$conversationData")
+        observer?.notify("Listing messages...")
         conversationData.uri?.let { uri ->
             DocumentFile.fromTreeUri(context, uri)?.let { folder ->
                 val parser = MessagesParser(dbHelper)
-                for (child in folder.listFiles()) {
+                val children = folder.listFiles()
+                var childrenDone = 0
+                val childrenCount = children.size
+                for (child in children) {
                     if (child.isFile) {
                         context.contentResolver.openInputStream(child.uri)?.let {
                             result.conversation = parser.readJson(it)
                         }
                     }
+                    childrenDone++
+                    observer?.notifyProgress(
+                        searchConversationCost +
+                                listMessagesCost * childrenDone / childrenCount
+                    )
                 }
 
                 folder.findFile(Paths.Messages.Folder.PHOTOS)?.let { photosFolder ->
                     result.photoFolderUri = photosFolder.uri
                 }
+                observer?.notifyProgress(searchConversationCost + listMessagesCost)
             }
         }
     }
@@ -60,4 +76,11 @@ class LoadConversationStatsTask(
         var conversationStats: ConversationStats? = null,
         var photoFolderUri: Uri? = null
     )
+
+    companion object {
+        const val TAG = "LoadConversationStatsTask"
+        const val searchConversationCost = 5
+        const val listMessagesCost = 85
+        const val findStatsCost = 10
+    }
 }
